b3c02ab47d971d3e8b9b4fc5f7c4bb49d00de3b1
Nishin_G <nishin@moontechnolabs.com>
2023-06-26 15:26:07 +0530

    first commit

---
+++ app/update-prompt/page.jsx
@@ -0,0 +1,60 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Form from "@/components/Form";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const EditPrompt = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  useEffect(() => {
+    const getPrompt = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
+      console.log("dataaaa", data);
+      setPost({ prompt: data.prompt, tag: data.tag });
+    };
+    if (promptId) getPrompt();
+  }, [promptId]);
+
+  const updatePrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+    if (!promptId) alert("Prompt ID is Missing");
+    try {
+      const response = await fetch(`api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/profile");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+  return (
+    <div>
+      <Form
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </div>
+  );
+};
+
+export default EditPrompt;
---
+++ models/user.js
@@ -0,0 +1,19 @@
+import { Schema, model, models } from 'mongoose'
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: [true, 'Email already exists'],
+        required: [true, "Email already in use"]
+    },
+    username: {
+        type: String,
+        required: [true, "Email already in use"],
+    },
+    image: {
+        type: String
+    }
+})
+
+const User = models?.User || model("User", UserSchema);
+export default User
